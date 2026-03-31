14e0669403311217bb1736bbe5cf0d6031afba74
glegle <39626338+rzxgle@users.noreply.github.com>
2026-03-31 16:24:07 -0300

    add display_name on labels_options.py

--- utils/label_options.py
+++ utils/label_options.py
@@ -4,24 +4,32 @@ def get_label_options():
             "Q1": {
                 "label": "EpicoPI1Legado",
                 "quarter": "Q1",
-                "year": 2026
+                "year": 2026,
+                "display_name": "Quarter 1",
+                "description": "Ciclo Q1 Afya Bridge"
             },
             "Q2": {
                 "label": "EpicoPI2Legado",
                 "quarter": "Q2",
-                "year": 2026
+                "year": 2026,
+                "display_name": "Quarter 2",
+                "description": "Ciclo Q2 Afya Bridge"
             }
         },
         "Afya One": {
             "Q1": {
                 "label": "PI1AfyaOne",
                 "quarter": "Q1",
-                "year": 2026
+                "year": 2026,
+                "display_name": "Quarter 1",
+                "description": "Ciclo Q1 Afya One"
             },
             "Q2": {
                 "label": "PI2AfyaOne",
                 "quarter": "Q2",
-                "year": 2026
+                "year": 2026,
+                "display_name": "Quarter 2",
+                "description": "Ciclo Q2 Afya One"
             }
         }
     }
