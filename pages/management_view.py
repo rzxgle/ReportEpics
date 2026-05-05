4733ed24379fe5278ad04deaf29d98de1d539291
glegle <39626338+rzxgle@users.noreply.github.com>
2026-05-05 10:47:03 -0300

    retirando visao de dependencia no roadmap

--- pages/management_view.py
+++ pages/management_view.py
@@ -57,11 +57,9 @@ df = issues_to_dataframe(issues)
 
 epic_progress = calculate_epic_progress(df)
 
-epic_df_owner = epic_df.rename(columns={"team": "epic_owner_team"})
-
-epic_progress = epic_progress.merge(
-    epic_df_owner,
-    on="epic",
+epic_progress = epic_df.merge(
+    epic_progress,
+    on=["team", "epic"],
     how="left"
 )
 
